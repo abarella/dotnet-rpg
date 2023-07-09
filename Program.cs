03bdaae5b21074eafe13ed4656997ebc33637027
Alberto Barella Jr <alberto@abjinfo.com.br>
2023-07-08 21:52:57 -0300

    youtube Patrick

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//https://www.youtube.com/watch?v=9zJn3a7L1uE
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
